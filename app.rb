d808b40887f36839faf70453da6b5d4bd637ed7e
Clifton Regis <github email address>
2016-08-11 21:32:03 -0400

    form lab

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  # Add your post route and action below
+  post '/team' do
+    @name =params[:name]
+    @coach =params[:coach]
+    @point =params[:pg]
+    @shooting =params[:sg]
+    @small =params[:sf]
+    @power =params[:pf]
+    @center= params[:c] 
+
+    erb :team
+  end
 
 end
